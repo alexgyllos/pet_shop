63694f3b02367afd6d82783e20bb9899b88e9481
Alex Gyllos <alex.gyllos@gmail.com>
2020-03-14 13:17:55 +0000

    passes customer_pet_count test

--- pet_shop.rb
+++ pet_shop.rb
@@ -63,3 +63,7 @@ end
 def remove_customer_cash(id_of_customer, amount_of_cash)
     return id_of_customer[:cash] -= amount_of_cash
 end
+
+def customer_pet_count(id_of_customer)
+  id_of_customer[:pets].count
+end
