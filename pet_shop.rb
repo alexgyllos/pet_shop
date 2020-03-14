5a2b4df8070e7f8a5d4d80bae8d8657578baa556
Alex Gyllos <alex.gyllos@gmail.com>
2020-03-14 10:54:10 +0000

    passes find_pet_by_name__returns_pet test

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,20 +6,20 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(name, amount)
-  name[:admin][:total_cash] += amount
+def add_or_remove_cash(shop, amount)
+  shop[:admin][:total_cash] += amount
 end
 
-def pets_sold(name)
-  name[:admin][:pets_sold]
+def pets_sold(shop)
+  shop[:admin][:pets_sold]
 end
 
-def increase_pets_sold(name, amount)
-  name[:admin][:pets_sold] += amount
+def increase_pets_sold(shop, amount)
+  shop[:admin][:pets_sold] += amount
 end
 
-def stock_count(name)
-  name[:pets].count
+def stock_count(shop)
+  shop[:pets].count
 end
 
 def pets_by_breed(shop, breeds)
@@ -31,3 +31,14 @@ def pets_by_breed(shop, breeds)
   end
   return pets_by_breed
 end
+
+def find_pet_by_name(shop, name)
+  # pets_name = {}
+  for pets in shop[:pets]
+    if pets[:name] == name
+      return pets
+    end
+  end
+end
+
+# def add_pet_to_stock
